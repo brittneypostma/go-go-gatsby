a902368b3d435ad50b0d65ec8d19fb50ca75a39a
Brittney <brittney.postma@hotmail.com>
2020-06-24 10:14:14 -0400

    done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,31 +13,47 @@ const Contact = () => {
           <form
             method="post"
             netlify-honeypot="bot-field"
+            data-netlify-recaptcha="true"
             data-netlify="true"
             name="contact"
             action="/thank-you"
           >
-            <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="bot-field" aria-label="hidden field" />
+            <input
+              type="hidden"
+              name="form-name"
+              value="contact"
+              aria-label="hidden field"
+            />
             <label>
               Name
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" aria-label="name" />
             </label>
             <label>
               Email
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" aria-label="email" />
             </label>
             <label>
               Subject
-              <input type="text" name="subject" id="subject" />
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                aria-label="subject"
+              />
             </label>
             <label>
               Message
-              <textarea name="message" id="message" rows="5" />
+              <textarea
+                name="message"
+                id="message"
+                rows="5"
+                aria-label="message"
+              />
             </label>
             <div className="btns">
-              <input type="reset" value="Clear" />
-              <input type="submit" value="Submit" />
+              <input type="reset" value="Clear" aria-label="clear form" />
+              <input type="submit" value="Submit" aria-label="submit form" />
             </div>
           </form>
         </fieldset>
